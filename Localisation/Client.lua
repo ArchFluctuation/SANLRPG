6fcb5f758ac716e1e95022d7c4e2a7238323bc69
Callum Dawson <CallumDawson@users.noreply.github.com>
2017-06-02 11:37:18 +0200

    Added NL translations from Joanna.

--- Localisation/Client.lua
+++ Localisation/Client.lua
@@ -2,48 +2,59 @@ L10N = {
 	["USERNAME"] = {
 		ar = "اسم المستخدم",
 		en_US = "Username:",
-		fr = "Nom d'utilisateur:"
+		fr = "Nom d'utilisateur:",
+		nl = "Gebruikersnaam:"
 	},
 	["PASSWORD"] = {
 		ar = "كلمة السر",
 		en_US = "Password:",
-		fr = "Mot de passe:"
+		fr = "Mot de passe:",
+		nl = "Wachtwoord:"
 	},
 	["CONFIRM"] = {
 		ar = "تأكيد",
 		en_US = "Confirm:",
-		fr = "Confirmer:"
+		fr = "Confirmer:",
+		nl = "Bevestigen:"
 	},
 	["REMEMBER_PASSWORD"] = {
 		ar = "تذكر كلمة المرور",
 		en_US = "Remember my password",
-		fr = "Mémoriser mon mot de passe"
+		fr = "Mémoriser mon mot de passe",
+		nl = "Herriner mijn wachtwoord"
 	},
 	["LOGIN"] = {
 		ar = "تسجيل الدخول",
 		en_US = "Login",
-		fr = "Connexion"
+		fr = "Connexion",
+		nl = "Inloggen"
 	},
 	["REGISTER"] = {
 		ar = "انشاء حساب",
 		en_US = "Register",
-		fr = "Créer un compte"
+		fr = "Créer un compte",
+		nl = "Registreren"
 	},
 	["CANCEL"] = {
 		ar = "الغاء",
 		en_US = "Cancel",
-		fr = "Annuler"
+		fr = "Annuler",
+		nl = "Annuleren"
 	},
 	["LOGIN_FIELDS"] = {
-		en_US = "*ACCOUNT* Please enter a username and password."
+		en_US = "*ACCOUNT* Please enter a username and password.",
+		nl = "*ACCOUNT* Vul een gebruikersnaam en wachtwoord in."
 	},
 	["REGISTER_DISALLOWED"] = {
-		en_US = "The use of ' is not allowed in account names."
+		en_US = "The use of ' is not allowed in account names.",
+		nl = "Het gebruik van ' is niet toegestaan in account namen."
 	},
 	["REGISTER_MISMATCH"] = {
-		en_US = "*ACCOUNT* Your passwords do not match."
+		en_US = "*ACCOUNT* Your passwords do not match.",
+		nl = "*ACCOUNT* Je wachtwoorden komen niet overeen."
 	},
 	["REGISTER_FIELDS"] = {
-		en_US = "*ACCOUNT* Please fill in all fields."
+		en_US = "*ACCOUNT* Please fill in all fields.",
+		nl = "*ACCOUNT* Vul alle velden in."
 	}
 }
