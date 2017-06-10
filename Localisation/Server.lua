006b521e94026fba93974b85e77fccb6320a3a50
JoannaNL <xjoanna99@live.nl>
2017-06-10 21:12:59 +0200

    Added Arabic and Turkish translations.

--- Localisation/Server.lua
+++ Localisation/Server.lua
@@ -1,12 +1,14 @@
 L10N = {
     ["PLAYTIME"] = {
         en_US = "Playtime",
-		nl = "Speeltijd",
-			tr = "Oynama Süresi"
+	nl = "Speeltijd",
+	tr = "Oynama Süresi",
+	ar = "زمن اللعب"
     },
     ["TOTAL_PLAYTIME"] = {
         en_US = "Total Playtime",
-		nl = "Totale Speeltijd",
-			tr = "Toplam Oynama Süresi"
+	nl = "Totale Speeltijd",
+	tr = "Toplam Oynama Süresi",
+	ar = "مجموع زمن اللعب"
     }
 }
