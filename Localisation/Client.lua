b66111f13f7aaca33b261c8e40a7cd3d92250176
Callum Dawson <CallumDawson@users.noreply.github.com>
2017-06-10 20:21:39 +0100

    Create Client.lua

--- Localisation/Client.lua
+++ Localisation/Client.lua
@@ -77,5 +77,34 @@ L10N = {
 		en_US = "Fuel",
 		nl = "Benzine",
 		tr = "Yakıt"
+	},
+	["INFO_RULES"] = {
+		en_US = [[All players must follow the rules at all times, and failing to do so will result in a punishment as decided by the staff team. The ultimate punishment is a ban. If you are banned, you can appeal your ban on the forum (http://sanlrpg.net).
+
+Beware that nobody ever needs to know your password, including staff members. However, your email address may be used when recovering your account.
+
+1) DO NOT DEATHMATCH
+Deathmatching (DMing), also known as Random Deathmatching (RDMing), is where you kill another player without a reason. If a police officer were to kill a wanted criminal, this would NOT be DMing. However, if a pilot was to kill another pilot, this would be considered DMing. DMing is not allowed at any time.
+
+2) SPEAK ENGLISH IN MAIN/TEAM CHAT
+You must speak English in the mainchat and teamchat. However, you can speak your own language in any other chat, including localchat.
+
+3) DO NOT USE ANYTHING TO GET AN UNFAIR ADVANTAGE
+You are not allowed to use anything that gives you an unfair advantage over another player (such as cheats, trainers, and hacks), or that allows you to exploit the server scripts.
+
+4) DO NOT FLAME/GRIEF
+Any form of flaming (such as insulting or bullying) or griefing (such as being annoying, or destroying things on purpose) is not allowed. Dark humour is permitted so long as no other players are offended.
+
+5) DO NOT HARASS/STALK
+You are not allowed to constantly contact or follow players if they make it clear they do not want you to do so. You are also not allowed to constantly annoy staff with repetitive messages.
+
+6) DO NOT IMPERSONATE
+You are not allowed to pretend to be another player (even if they play on another server), and you are especially not allowed to pretend to be a member of staff.
+
+7) FOLLOW STAFF INSTRUCTIONS
+Staff may punish you for rulebreaking, even if the rule is not officially listed here. You must also follow any instructions given to you by a member of staff while they are performing their official duties. Staff can also choose to not enforce rules officially listed, where appropriate.
+
+8) DO NOT ATTACK POLICE TO BECOME WANTED
+You are not allowed to attack anybody on the Police team in order to become wanted. Instead, you should perform a criminal act using the job features.]]
 	}
 }
