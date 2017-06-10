377b7776e265363e28ae0f1cd74e3d2176031464
JoannaNL <xjoanna99@live.nl>
2017-06-10 20:44:02 +0200

    Create Client.lua
    Turkish translations added.

--- Localisation/Client.lua
+++ Localisation/Client.lua
@@ -3,62 +3,74 @@ L10N = {
 		ar = "اسم المستخدم",
 		en_US = "Username:",
 		fr = "Nom d'utilisateur:",
-		nl = "Gebruikersnaam:"
+		nl = "Gebruikersnaam:",
+		tr = "Kullanıcı Adı:"
 	},
 	["PASSWORD"] = {
 		ar = "كلمة السر",
 		en_US = "Password:",
 		fr = "Mot de passe:",
-		nl = "Wachtwoord:"
+		nl = "Wachtwoord:",
+		tr = "Şifre:"
 	},
 	["CONFIRM"] = {
 		ar = "تأكيد",
 		en_US = "Confirm:",
 		fr = "Confirmer:",
-		nl = "Bevestigen:"
+		nl = "Bevestigen:",
+		tr = "Onayla:"
 	},
 	["REMEMBER_PASSWORD"] = {
 		ar = "تذكر كلمة المرور",
 		en_US = "Remember my password",
 		fr = "Mémoriser mon mot de passe",
-		nl = "Herinner mijn wachtwoord"
+		nl = "Herinner mijn wachtwoord",
+		tr = "Şifremi hatırla"
 	},
 	["LOGIN"] = {
 		ar = "تسجيل الدخول",
 		en_US = "Login",
 		fr = "Connexion",
-		nl = "Inloggen"
+		nl = "Inloggen",
+		tr = "Giriş"
 	},
 	["REGISTER"] = {
 		ar = "انشاء حساب",
 		en_US = "Register",
 		fr = "Créer un compte",
-		nl = "Registreren"
+		nl = "Registreren",
+		tr = "Kayıt Ol"
 	},
 	["CANCEL"] = {
 		ar = "الغاء",
 		en_US = "Cancel",
 		fr = "Annuler",
-		nl = "Annuleren"
+		nl = "Annuleren",
+		tr = "İptal"
 	},
 	["LOGIN_FIELDS"] = {
 		en_US = "*ACCOUNT* Please enter a username and password.",
-		nl = "*ACCOUNT* Vul een gebruikersnaam en wachtwoord in."
+		nl = "*ACCOUNT* Vul een gebruikersnaam en wachtwoord in.",
+		tr = "* HESAP Lütfen bir kullanıcı adı ve şifre girin"
 	},
 	["REGISTER_DISALLOWED"] = {
 		en_US = "The use of ' is not allowed in account names.",
-		nl = "Het gebruik van ' is niet toegestaan in account namen."
+		nl = "Het gebruik van ' is niet toegestaan in account namen.",
+		tr = "Kullanıcı adları ' işareti içeremez."
 	},
 	["REGISTER_MISMATCH"] = {
 		en_US = "*ACCOUNT* Your passwords do not match.",
-		nl = "*ACCOUNT* Je wachtwoorden komen niet overeen."
+		nl = "*ACCOUNT* Je wachtwoorden komen niet overeen.",
+		tr = "* HESAP Şifreleriniz eşleşmiyor."
 	},
 	["REGISTER_FIELDS"] = {
 		en_US = "*ACCOUNT* Please fill in all fields.",
-		nl = "*ACCOUNT* Vul alle velden in."
+		nl = "*ACCOUNT* Vul alle velden in.",
+		tr = "* HESAP Lütfen bütün boşlukları doldurun."
 	},
 	["FUEL"] = {
 		en_US = "Fuel",
-		nl = "Benzine"
+		nl = "Benzine",
+		tr = "-Yakıt"
 	}
 }
