ffe5fbca77c040af308941d44cb37319aaa691b4
Callum Dawson <CallumDawson@users.noreply.github.com>
2017-06-12 13:59:46 +0100

    Create Server.lua

--- Localisation/Server.lua
+++ Localisation/Server.lua
@@ -10,5 +10,17 @@ L10N = {
         en_US = "Total Playtime",
 	nl = "Totale Speeltijd",
 	tr = "Toplam Oynama Süresi"
+    },
+    ["VENDING_PURCHASE"] = {
+        en_US = "*VENDING* Press H to purchase a drink."
+    },
+    ["VENDING_PURCHASED"] = {
+        en_US = "*VENDING* You purchased a drink for $%s."
+    },
+    ["VENDING_MONEY"] = {
+        en_US = "*VENDING* You do not have enough money."
+    },
+    ["DUTY"] = {
+        en_US = "You are now on %s duty. Resign to change job."
     }
 }
