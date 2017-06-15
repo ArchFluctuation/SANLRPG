6f5995ffc111e9df4f863e3de7e5cf964bff1fb2
JoannaNL <xjoanna99@live.nl>
2017-06-15 20:56:50 +0200

    Added Dutch, Portuguese and Spanish translations.

--- Localisation/Server.lua
+++ Localisation/Server.lua
@@ -2,25 +2,33 @@ L10N = {
     ["PLAYTIME"] = {
 	ar = "زمن اللعب",
         en_US = "Playtime",
+	es = "Tiempo jugado",
 	nl = "Speeltijd",
+	pt = "Tempo Jogado",
 	tr = "Oynama Süresi"
     },
     ["TOTAL_PLAYTIME"] = {
 	ar = "مجموع زمن اللعب",
         en_US = "Total Playtime",
+	es = "Total de tiempo jugado",
 	nl = "Totale Speeltijd",
+	pt = "Total de Tempo Jogado",
 	tr = "Toplam Oynama Süresi"
     },
     ["VENDING_PURCHASE"] = {
-        en_US = "*VENDING* Press H to purchase a drink."
+        en_US = "*VENDING* Press H to purchase a drink.",
+	nl = "*AUTOMAAT* Druk op H om een drankje te kopen."
     },
     ["VENDING_PURCHASED"] = {
         en_US = "*VENDING* You purchased a drink for $%s."
+	nl = "*AUTOMAAT* Je hebt een drankje gekocht voor $%s."
     },
     ["VENDING_MONEY"] = {
-        en_US = "*VENDING* You do not have enough money."
+        en_US = "*VENDING* You do not have enough money.",
+	nl = "*AUTOMAAT* Je hebt niet genoeg geld."
     },
     ["DUTY"] = {
-        en_US = "You are now on %s duty. Resign to change job."
+        en_US = "You are now on %s duty. Resign to change job.",
+	nl = "Je bent nu in dienst als %s. Neem ontslag om je baan te veranderen."
     }
 }
