43c79734a987f5b80089a41524fbd309fa907aeb
JoannaNL <xjoanna99@live.nl>
2017-06-15 20:46:45 +0200

    Added Spanish, Portuguese and Dutch translations.

--- Localisation/Client.lua
+++ Localisation/Client.lua
@@ -2,93 +2,121 @@ L10N = {
 	["USERNAME"] = {
 		ar = "اسم المستخدم",
 		en_US = "Username:",
+		es = "Nombre de usuario:",
 		fr = "Nom d'utilisateur:",
 		nl = "Gebruikersnaam:",
+		pt = "Nome de utilizador:",
 		tr = "Kullanıcı Adı:"
 	},
 	["PASSWORD"] = {
 		ar = "كلمة السر",
 		en_US = "Password:",
+		es = "Contraseña:",
 		fr = "Mot de passe:",
 		nl = "Wachtwoord:",
+		pt = "Senha:",
 		tr = "Şifre:"
 	},
 	["CONFIRM"] = {
 		ar = "تأكيد",
 		en_US = "Confirm:",
+		es = "Confirmar:",
 		fr = "Confirmer:",
 		nl = "Bevestigen:",
+		pt = "Confirmação:",
 		tr = "Onayla:"
 	},
 	["REMEMBER_PASSWORD"] = {
 		ar = "تذكر كلمة المرور",
 		en_US = "Remember my password",
+		es = "Recordar mi contraseña",
 		fr = "Mémoriser mon mot de passe",
 		nl = "Herinner mijn wachtwoord",
+		pt = "Guardar a minha password",
 		tr = "Şifremi hatırla"
 	},
 	["LOGIN"] = {
 		ar = "تسجيل الدخول",
 		en_US = "Login",
+		es = "Iniciar sesión",
 		fr = "Connexion",
 		nl = "Inloggen",
+		pt = "Iniciar Sessão",
 		tr = "Giriş"
 	},
 	["REGISTER"] = {
 		ar = "انشاء حساب",
 		en_US = "Register",
+		es = "Registrarse",
 		fr = "Créer un compte",
 		nl = "Registreren",
+		pt = "Registar-se",
 		tr = "Kayıt Ol"
 	},
 	["CANCEL"] = {
 		ar = "الغاء",
 		en_US = "Cancel",
+		es = "Cancelar"
 		fr = "Annuler",
 		nl = "Annuleren",
+		pt = "Cancelar",
 		tr = "İptal"
 	},
 	["LOGIN_FIELDS"] = {
 		ar = "حساب* الرجاء ادخال اسم  اسم الحساب و الكلمه السريه*",
 		en_US = "*ACCOUNT* Please enter a username and password.",
+		es = "*CUENTA* Porfavor ingrese un nombre de usuario y contraseña.",
 		nl = "*ACCOUNT* Vul een gebruikersnaam en wachtwoord in.",
+		pt = "*CONTA* Por favor insere um nome de utilizador e senha.",
 		tr = "*HESAP* Lütfen bir kullanıcı adı ve şifre girin."
 	},
 	["REGISTER_MISMATCH"] = {
 		ar = "*كلمتك السريه لا تتطابق*حساب",
 		en_US = "*ACCOUNT* Your passwords do not match.",
+		es = "*CUENTA* Tus contraseñas no coinciden.",
 		nl = "*ACCOUNT* Je wachtwoorden komen niet overeen.",
+		pt = "*CONTA* A tua senha não é correspondida.",
 		tr = "*HESAP* Şifreleriniz eşleşmiyor."
 	},
 	["REGISTER_FIELDS"] = {
 		ar = "حساب*الرجاء املاء كل المجلات*",
 		en_US = "*ACCOUNT* Please fill in all fields.",
+		es = "*CUENTA* Porfavor rellena todos los campos.",
 		nl = "*ACCOUNT* Vul alle velden in.",
+		pt = "*CONTA* Por favor preenche todos os campos.",
 		tr = "*HESAP* Lütfen bütün boşlukları doldurun."
 	},
 	["INTRO_WELCOME_LS"] = {
-		en_US = "Welcome to Los Santos International Airport."
+		en_US = "Welcome to Los Santos International Airport.",
+		nl = "Welkom in Los Santos' Internationale Vliegveld."
 	},
 	["INTRO_JOB"] = {
-		en_US = "The first thing you should do is get a job."
+		en_US = "The first thing you should do is get a job.",
+		nl = "Het eerste dat je moet doen is een baan vinden."
 	},
 	["INTRO_BLIPS"] = {
-		en_US = "Jobs are shown as yellow circles on the map."
+		en_US = "Jobs are shown as yellow circles on the map.",
+		nl = "Beroepen worden aangegeven met gele cirkels op de map."
 	},
 	["FUEL"] = {
 		ar = "وقود",
 		en_US = "Fuel",
+		es = "Gasolina",
 		nl = "Benzine",
+		pt = "Combustível",
 		tr = "Yakıt"
 	},
 	["RESPAWN_HOSPITAL"] = {
-		en_US = "You were taken to %s."
+		en_US = "You were taken to %s.",
+		nl = "Je bent naar %s gebracht."
 	},
 	["RESPAWN_REPLENISH_HEALTH"] = {
-		en_US = "There are various places and people that can help replenish lost health."
+		en_US = "There are various places and people that can help replenish lost health.",
+		nl = "Er zijn verschillende plaatsen en mensen die je kunnen helpen met het bijvullen van je gezondheid."
 	},
 	["RESPAWN_REDUCE_VISITS"] = {
-		en_US = "To reduce your hospital visits."
+		en_US = "To reduce your hospital visits.",
+		nl = "Om je ziekenhuis bezoeken te verminderen."
 	},
 	["INFO_RULES"] = {
 		ar = [[كل اللاعبين يجب أن يتبعوا القوانين في كل الاوقات .. اذا لم يفعلوا ذلك سوف يتم عقابهم بعقاب محدد من قبل مسؤولين السيرفر ، اقصى عقاب هو أن يتم حظرك من السيرفر 
@@ -147,6 +175,89 @@ You are not allowed to pretend to be another player (even if they play on anothe
 Staff may punish you for rulebreaking, even if the rule is not officially listed here. You must also follow any instructions given to you by a member of staff while they are performing their official duties. Staff can also choose to not enforce rules officially listed, where appropriate.
 
 8) DO NOT ATTACK POLICE TO BECOME WANTED
-You are not allowed to attack anybody on the Police team in order to become wanted. Instead, you should perform a criminal act using the job features.]]
+You are not allowed to attack anybody on the Police team in order to become wanted. Instead, you should perform a criminal act using the job features.]],
+		es = [[Todos los jugadores deben de seguir las reglas siempre, y romperlas traerá consigo un castigo (ban). Si estás baneado, puedes apelar tu baneo en el foro (http://san1rpg.net)
+Asegúrate de que nadie sepa tu contraseña, incluyendo a miembros staff. Además, tu email puede ser utilizado para recuperar tu cuenta.
+
+1) DEATHMATCH no permitido
+Deathmatching, también conocido como Randon Deathtmatching, consiste en matar a otro jugador sin razón alguna.Si un agente de policía mata a un criminal en busca y captura, esto no sería Dming. Sin embargo, si un piloto mata a otro piloto, esto se consideraría Dming. Dming no está permitido.
+
+2) Hablar inglés en el chat principal/equipo
+Debes de hablar inglés en el chat principal y en el chat de grupo. Sin embargo, puedes hablar tu propio idioma en cualquier otro chat, incluyendo localchat.
+
+3) No utilices nada para conseguir ventaja suciamente
+ No está permitido el uso de cualquier cosa que te ofrezca una injusta ventaja sobre otro jugador (como trucos, trainers y hacks) o que te permita meter exploits a los scripts del server.
+
+4) No molestes/insultes
+ Cualquier tipo de insultos o molestias no están permitidas. El humor negro está permitido siempre que los otros jugadores no se ofendan.
+
+5) No acoses
+No tienes permitido el contacto constante o seguir a los jugadores si te dejan claro que no quieren que lo hagas. Tampoco tienes permitido el molestar constantemente a los staffs con mensajes repetitivos.
+
+6) No personifiques
+No está permitido el pretender ser otro jugador(aunque jueguen en otro servidor) tampoco puedes pretender ser un miembro de staff
+
+7) Sigue las instrucciones de los staff
+ Los staffs pueden castigarte por romper las normas, incluso aunque la norma no esté oficialmente escrita aquí. También debes seguir cualquier instrucción dada por un miembro staff mientras estén realizando su trabajo oficial. Los staff pueden también elegir no hacer cumplir las normas oficiales escritas aquí.
+
+ 8) No ataques a la policía para ser buscado
+ No está permitido atacar a nadie del equipo de la policía para ser buscado. En lugar de eso, deberías de realizar un acto criminal utilizando las características de cada trabajo.]],
+		nl = [[Alle spelers moeten de regels altijd volgen, hierin falen zal resulteren in een straf die bepaald word door het staff team. De uiterste straf is een ban. Als je gebannen bent, kun je hiertegen in beroep gaan op het forum (http://sanlrpg.net).
+ 
+Pas op dat niemand jouw wachtwoord weet, inclusief staff leden. Hoe dan ook, jouw email adres kan gebruikt worden wanneer je jouw account wilt herstellen.
+ 
+ 
+1) DOE NIET AAN DEATHMATCHING
+Deathmatching (DMing), ook wel bekend als Random Deathmatching (RDMing), is het doden van een andere speler zonder duidelijke reden. Als een politie agent een gezochte crimineel wilde doden, zou dit geen DMing zijn. Hoe dan ook, als een piloot een andere piloot wilt doden, zou dit gezien worden als DMing. DMing is nooit toegestaan.
+ 
+2) SPREEK ENGELS IN MAIN/TEAM CHAT
+Je moet Engels spreken in de mainchat en in teamchat. Hoe dan ook, je kunt jouw eigen taal spreken in elke andere chat, inclusief de lokale chat.
+ 
+3) GEBRUIK NIKS WAT JOUW EEN ONEERLIJK VOORDEEL KAN GEVEN
+Je bent niet bevoegd om iets te gebruiken wat jouw een oneerlijk voordeel kan geven (bijvoorbeeld cheats en hacks) of wat je in staat stelt om de server scripts te exploiteren.
+ 
+4) SCHELD OF KWEL NIET.
+Elke vorm van schelden (zoals uitschelden of pesten) of kwelling (zoals irritant zijn, of bewust dingen vernietigen) is niet toegestaan. Duistere humor is toegestaan zolang andere spelers zich niet beledigd voelen hierdoor.
+ 
+5) VAL NIET LASTIG EN STALK NIET
+Je bent niet bevoegd om constant contact te zoeken met spelers en/of spelers te volgen als zij duidelijk hebben aangegeven dit niet te willen. Je bent ook niet bevoegd om continu staff leden lastig te vallen met herhalende berichten.
+ 
+6) DOE JE NIET VOOR ALS IEMAND ANDERS
+Je bent niet bevoegd om je voor te doen als een andere speler (zelfs als deze speler op een andere server speelt), en je bent vooral niet bevoegd om je voor te doen als een staff lid.
+ 
+7) VOLG STAFF INSTRUCTIES
+Staff leden kunnen je een straf geven voor het breken van regels, zelfs als de regel hier niet officieel genoemd is. Je moet ook de instructies opvolgen gegeven door een staff lid dat bezig is met het uitvoeren van zijn/haar officiële taken. Staff leden kunnen er ook voor kiezen om de officieel benoemde regels niet na te streven, wanneer dit passend is.
+ 
+8) VAL DE POLITIE NIET AAN OM GEZOCHT TE WORDEN
+Je bent niet toegestaan om iemand in het politie team aan te vallen zodat je gezocht word. In plaats daarvan, je zou een criminele activiteit moeten uitvoeren door de gegeven mogelijkheden te gebruiken.]],
+		pt = [[Todos os jogadores devem seguir as regras em todas as alturas, e se não o fizerem serão sujeitos a uma punição por parte da equipa STAFF. A punição de maior valor será um ban. No caso de estares banido poderás apelar ao teu ban no fórum (http://sanlrpg.net).
+ 
+Atenção que ninguém tem de saber a tua senha, nem mesmo os membros da equipa STAFF. Contudo, o teu email poderá ser usado para se recuperar a tua conta.
+ 
+1) DEATHMATCH não é permitido
+Deathmatching (DMing), também conhecido como Random Deathmatching (RDMing), consiste em matar outro jogador sem nenhuma razão. Se um polícia matasse um criminoso procurado, isto não seria visto como DMing. Contudo, se um piloto matasse outro piloto, isto seria DMing. DMing é estritamente proibido em qualquer situação.
+ 
+2) Fala Inglês no chat principal ou chat de equipa
+Deves falar Inglês no chat principal e no chat de equipa. Contudo, podes falar a tua própria língua nos tipos de chat restantes, incluindo o chat local.
+ 
+3) Não uses nada para conseguires uma vantagem injusta
+Não é permitido usares nada que te dê uma vantagem injusta sobre outros jogadores (como cheats ou hacks), ou que te permita explorar os scripts do servidor.
+ 
+4) Não insultes/perturbes
+Qualquer forma de insulto/bullying ou perturbo (como por exemplo, ser chato ou destruir coisas de propósito) não é permitido.
+Humor negro é permitido mas apenas se nenhuma outra pessoa ficar ofendida.
+ 
+5) Não incomodes/sigas
+Não te é permitido seguires ou contactares as outras pessoas constantemente se elas te tiverem pedido para não o fazeres. Também não te é permitido mandares várias mensagens repetitivas aos membros da equipa STAFF.
+ 
+6) Não assumas a identidade de outra pessoa
+Não te é permitido assumires a identidade de outra pessoa (mesmo que a pessoa jogue noutro servidor), e, mais importante ainda, não te é permitido assumires a identidade de um membro da equipa STAFF.
+ 
+7) Segue as instruções da equipa STAFF
+A equipa STAFF pode castigar-te por não obedeceres às regras, mesmo que a regra não esteja oficialmente listada aqui. Deves de cumprir quaisquer instruções que um membro da STAFF te dê enquanto o mesmo estiver a exercer os seus deveres oficiais. A equipa STAFF também pode escolher exercer regras que não estejam listadas oficialmente se for mesmo necessário.
+ 
+8) Não ataques a polícia para ficares como 'procurado'
+Não te é permitido atacares um membro da polícia para ficares como 'procurado'. Em vez disso, deves de exercer uma atividade criminal usando as várias ferramentas do 'trabalho'.]]
+		
 	}
 }
