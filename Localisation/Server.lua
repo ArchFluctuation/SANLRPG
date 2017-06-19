0dad282c505a70f2d17e3fa139946558127c5b39
ArchFluctuation <orange@flucado.net>
2017-06-19 13:53:29 -0300

    Update Server.lua

--- Localisation/Server.lua
+++ Localisation/Server.lua
@@ -4,7 +4,7 @@ L10N = {
 	en_US = "Playtime",
 	es = "Tiempo jugado",
 	nl = "Speeltijd",
-	pt = "Tempo Jogado",
+	pt_BR = "Tempo Jogado",
 	tr = "Oynama Süresi"
     },
     ["TOTAL_PLAYTIME"] = {
@@ -12,91 +12,91 @@ L10N = {
 	en_US = "Total Playtime",
 	es = "Total de tiempo jugado",
 	nl = "Totale Speeltijd",
-	pt = "Total de Tempo Jogado",
+	pt_BR = "Total de Tempo Jogado",
 	tr = "Toplam Oynama Süresi"
     },
     ["VENDING_PURCHASE"] = {
 	en_US = "*VENDING* Press H to purchase a drink.",
 	nl = "*AUTOMAAT* Druk op H om een drankje te kopen.",
-	pt = "*MÁQUINA* Pressione H para comprar uma bebida."
+	pt_BR = "*MÁQUINA* Pressione H para comprar uma bebida."
     },
     ["VENDING_PURCHASED"] = {
 	en_US = "*VENDING* You purchased a drink for $%s.",
 	nl = "*AUTOMAAT* Je hebt een drankje gekocht voor $%s.",
-	pt = "*MÁQUINA* Você comprou uma bebida por $%s."
+	pt_BR = "*MÁQUINA* Você comprou uma bebida por $%s."
     },
     ["VENDING_MONEY"] = {
 	en_US = "*VENDING* You do not have enough money.",
 	nl = "*AUTOMAAT* Je hebt niet genoeg geld.",
-	pt = "*MÁQUINA* Você não tem dinheiro o bastante."
+	pt_BR = "*MÁQUINA* Você não tem dinheiro o bastante."
     },
     ["DUTY"] = {
 	en_US = "You are now on %s duty. Resign to change job.",
     	nl = "Je bent nu in dienst als %s. Neem ontslag om je baan te veranderen.",
-	pt = "Você está em serviço como %s. Demita-se para mudar de emprego."
+	pt_BR = "Você está em serviço como %s. Demita-se para mudar de emprego."
     },
     ["VEHICLE_RENTED_BY"] = {
 	en_US = "*VEHICLE* This vehicle is rented by %s.",
-	pt = "*VEÍCULO* Este veículo está alugado por %s."
+	pt_BR = "*VEÍCULO* Este veículo está alugado por %s."
     },
     ["VEHICLE_RESTRICTED"] = {
 	en_US = "*VEHICLE* This vehicle is restricted to %s.",
-	pt = "*VEÍCULO* Este veículo está restrito a %s."
+	pt_BR = "*VEÍCULO* Este veículo está restrito a %s."
     },
     ["PASSWORD_INCORRECT"] = {
 	en_US = "*ACCOUNT* Your password is incorrect.",
-	pt = "*CONTA* Sua senha está incorreta."
+	pt_BR = "*CONTA* Sua senha está incorreta."
     },
     ["PASSWORD_NOT_EXIST"] = {
 	en_US = "*ACCOUNT* This username does not exist.",
-	pt = "*CONTA* Este nome de usuário não existe."
+	pt_BR = "*CONTA* Este nome de usuário não existe."
     },
     ["ACCOUNT_CREATED"] = {
 	en_US = "*ACCOUNT* Account creation successful!",
-	pt = "*CONTA* Conta criada com sucesso!"
+	pt_BR = "*CONTA* Conta criada com sucesso!"
     },
     ["UNEXPECTED_ERROR"] = {
 	en_US = "*ACCOUNT* An unexpected error has occured!",
-	pt = "*CONTA* Um erro inesperado ocorreu!"
+	pt_BR = "*CONTA* Um erro inesperado ocorreu!"
     },
     ["ACCOUNT_EXISTS"] = {
 	en_US = "*ACCOUNT* This username already exists.",
-	py = "*CONTA* Este nome de usuário já existe."
+	pt_BR = "*CONTA* Este nome de usuário já existe."
     },
     ["ACCOUNT_BLOCKED"] = {
 	en_US = "*ACCOUNT* This username is blocked.",
-	pt = "*CONTA* Este nome de usuário está bloqueado."
+	pt_BR = "*CONTA* Este nome de usuário está bloqueado."
     },
     ["HELP_KEY"] = {
 	en_US = "*NOTICE* PLEASE PRESS THE F9 KEY FOR HELP,
-	pt = "*AVISO* PRESSIONE A TECLA F9 PARA VER AJUDA."
+	pt_BR = "*AVISO* PRESSIONE A TECLA F9 PARA VER AJUDA."
     },
     ["JOB_HINT_NATIONAL_GUARD"] = {
 	en_US = "*JOB* Hunt down and kill players in the 'Mafia' team.",
-	pt = "*EMPREGO* Persiga e mate jogadores no time da 'Mafia'."
+	pt_BR = "*EMPREGO* Persiga e mate jogadores no time da 'Mafia'."
     },
     ["JOB_HINT_MAFIA"] = {
 	en_US = "*JOB* Hunt down and kill players in the 'National Guard' team.",
-	pt = "*EMPREGO* Persiga e mate jogadores no time da 'National Guard'."
+	pt_BR = "*EMPREGO* Persiga e mate jogadores no time da 'National Guard'."
     },
     ["JOB_HINT_POLICE_OFFICER"] = {
 	en_US = "*JOB* Arrest wanted players (press the F9 key for more information).",
-	pt = "*EMPREGO* Prenda jogadores procurados (pressione a tecla F9 para mais informações)."
+	pt_BR = "*EMPREGO* Prenda jogadores procurados (pressione a tecla F9 para mais informações)."
     },
     ["JOBS_EMPLOYMENT_START"] = {
 	en_US = "*EMPLOYMENT* You are now employed as a %s!",
-	pt = "*EMPREGO* Você agora está empregado como %s!"
+	pt_BR = "*EMPREGO* Você agora está empregado como %s!"
     },
     ["JOBS_EMPLOYMENT_RESIGN"] = {
 	en_US = "*EMPLOYMENT* You have resigned as a %s.",
-	pt = "*EMPREGO* Você se demitiu do emprego como %s."
+	pt_BR = "*EMPREGO* Você se demitiu do emprego como %s."
     },
     ["CURRENT_PEAK"] = {
 	en_US = "*PEAK* The current player peak is %s players.",
-	pt = "*PICO* O atual pico de número de jogares é de %s."
+	pt_BR = "*PICO* O atual pico de número de jogares é de %s."
     },
     ["NEW_PEAK"] = {
 	en_US = "*PEAK* A new player peak of %s players has been reached!",
-	pt = "*PICO* Um novo pico de %s jogadores foi alcançado!"
+	pt_BR = "*PICO* Um novo pico de %s jogadores foi alcançado!"
     }
 }
