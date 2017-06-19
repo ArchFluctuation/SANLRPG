065baab12232249ca233740c9a49bd689e9a37e0
ArchFluctuation <orange@flucado.net>
2017-06-19 09:56:50 -0300

    Portuguese update for Server.lua
    Additionally I have changed "en_US" lines to be indented with tabs, as every other line is (within the tables).

--- Localisation/Server.lua
+++ Localisation/Server.lua
@@ -1,7 +1,7 @@
 L10N = {
     ["PLAYTIME"] = {
 	ar = "زمن اللعب",
-        en_US = "Playtime",
+	en_US = "Playtime",
 	es = "Tiempo jugado",
 	nl = "Speeltijd",
 	pt = "Tempo Jogado",
@@ -9,74 +9,94 @@ L10N = {
     },
     ["TOTAL_PLAYTIME"] = {
 	ar = "مجموع زمن اللعب",
-        en_US = "Total Playtime",
+	en_US = "Total Playtime",
 	es = "Total de tiempo jugado",
 	nl = "Totale Speeltijd",
 	pt = "Total de Tempo Jogado",
 	tr = "Toplam Oynama Süresi"
     },
     ["VENDING_PURCHASE"] = {
-        en_US = "*VENDING* Press H to purchase a drink.",
-	nl = "*AUTOMAAT* Druk op H om een drankje te kopen."
+	en_US = "*VENDING* Press H to purchase a drink.",
+	nl = "*AUTOMAAT* Druk op H om een drankje te kopen.",
+	pt = "*MÁQUINA* Pressione H para comprar uma bebida."
     },
     ["VENDING_PURCHASED"] = {
-        en_US = "*VENDING* You purchased a drink for $%s."
-	nl = "*AUTOMAAT* Je hebt een drankje gekocht voor $%s."
+	en_US = "*VENDING* You purchased a drink for $%s.",
+	nl = "*AUTOMAAT* Je hebt een drankje gekocht voor $%s.",
+	pt = "*MÁQUINA* Você comprou uma bebida por $%s."
     },
     ["VENDING_MONEY"] = {
-        en_US = "*VENDING* You do not have enough money.",
-	nl = "*AUTOMAAT* Je hebt niet genoeg geld."
+	en_US = "*VENDING* You do not have enough money.",
+	nl = "*AUTOMAAT* Je hebt niet genoeg geld.",
+	pt = "*MÁQUINA* Você não tem dinheiro o bastante."
     },
     ["DUTY"] = {
-        en_US = "You are now on %s duty. Resign to change job.",
-    	nl = "Je bent nu in dienst als %s. Neem ontslag om je baan te veranderen."
+	en_US = "You are now on %s duty. Resign to change job.",
+    	nl = "Je bent nu in dienst als %s. Neem ontslag om je baan te veranderen.",
+	pt = "Você está em serviço como %s. Demita-se para mudar de emprego."
     },
     ["VEHICLE_RENTED_BY"] = {
-        en_US = "*VEHICLE* This vehicle is rented by %s."
+	en_US = "*VEHICLE* This vehicle is rented by %s.",
+	pt = "*VEÍCULO* Este veículo está alugado por %s."
     },
     ["VEHICLE_RESTRICTED"] = {
-        en_US = "*VEHICLE* This vehicle is restricted to %s."
+	en_US = "*VEHICLE* This vehicle is restricted to %s.",
+	pt = "*VEÍCULO* Este veículo está restrito a %s."
     },
     ["PASSWORD_INCORRECT"] = {
-        en_US = "*ACCOUNT* Your password is incorrect."
+	en_US = "*ACCOUNT* Your password is incorrect.",
+	pt = "*CONTA* Sua senha está incorreta."
     },
     ["PASSWORD_NOT_EXIST"] = {
-        en_US = "*ACCOUNT* This username does not exist."
+	en_US = "*ACCOUNT* This username does not exist.",
+	pt = "*CONTA* Este nome de usuário não existe."
     },
     ["ACCOUNT_CREATED"] = {
-        en_US = "*ACCOUNT* Account creation successful!"
+	en_US = "*ACCOUNT* Account creation successful!",
+	pt = "*CONTA* Conta criada com sucesso!"
     },
     ["UNEXPECTED_ERROR"] = {
-        en_US = "*ACCOUNT* An unexpected error has occured!"
+	en_US = "*ACCOUNT* An unexpected error has occured!",
+	pt = "*CONTA* Um erro inesperado ocorreu!"
     },
     ["ACCOUNT_EXISTS"] = {
-        en_US = "*ACCOUNT* This username already exists."
+	en_US = "*ACCOUNT* This username already exists.",
+	py = "*CONTA* Este nome de usuário já existe."
     },
     ["ACCOUNT_BLOCKED"] = {
-        en_US = "*ACCOUNT* This username is blocked."
+	en_US = "*ACCOUNT* This username is blocked.",
+	pt = "*CONTA* Este nome de usuário está bloqueado."
     },
     ["HELP_KEY"] = {
-        en_US = "*NOTICE* PLEASE PRESS THE F9 KEY FOR HELP."
+	en_US = "*NOTICE* PLEASE PRESS THE F9 KEY FOR HELP,
+	pt = "*AVISO* PRESSIONE A TECLA F9 PARA VER AJUDA."
     },
     ["JOB_HINT_NATIONAL_GUARD"] = {
-        en_US = "*JOB* Hunt down and kill players in the 'Mafia' team."
+	en_US = "*JOB* Hunt down and kill players in the 'Mafia' team.",
+	pt = "*EMPREGO* Persiga e mate jogadores no time da 'Mafia'."
     },
     ["JOB_HINT_MAFIA"] = {
-        en_US = "*JOB* Hunt down and kill players in the 'National Guard' team."
+	en_US = "*JOB* Hunt down and kill players in the 'National Guard' team.",
+	pt = "*EMPREGO* Persiga e mate jogadores no time da 'National Guard'."
     },
     ["JOB_HINT_POLICE_OFFICER"] = {
-        en_US = "*JOB* Arrest wanted players (press the F9 key for more information)."
+	en_US = "*JOB* Arrest wanted players (press the F9 key for more information).",
+	pt = "*EMPREGO* Prenda jogadores procurados (pressione a tecla F9 para mais informações)."
     },
     ["JOBS_EMPLOYMENT_START"] = {
-        en_US = "*EMPLOYMENT* You are now employed as a %s!"
+	en_US = "*EMPLOYMENT* You are now employed as a %s!",
+	pt = "*EMPREGO* Você agora está empregado como %s!"
     },
     ["JOBS_EMPLOYMENT_RESIGN"] = {
-        en_US = "*EMPLOYMENT* You have resigned as a %s.";
+	en_US = "*EMPLOYMENT* You have resigned as a %s.",
+	pt = "*EMPREGO* Você se demitiu do emprego como %s."
     },
     ["CURRENT_PEAK"] = {
-        en_US = "*PEAK* The current player peak is %s players."
+	en_US = "*PEAK* The current player peak is %s players.",
+	pt = "*PICO* O atual pico de número de jogares é de %s."
     },
     ["NEW_PEAK"] = {
-        en_US = "*PEAK* A new player peak of %s players has been reached!"
+	en_US = "*PEAK* A new player peak of %s players has been reached!",
+	pt = "*PICO* Um novo pico de %s jogadores foi alcançado!"
     }
 }
